abb47a1e3e9b8155dbc8d911d2468d1f038e0ae6
nacaceres <35931488+nacaceres@users.noreply.github.com>
2019-08-20 07:08:05 -0500

    Cambio de var por let y const
    Se realiza el cambio por recomendación de John en clase.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 "use strict";
 
-
+// Nicolás Cáceres: Recuerda que John nos dijo que es mejor el uso de let y const que el de var:
 
 //This is how we acccess the container that we will 'type' into.
-var container = document.getElementById("content");
+let container = document.getElementById("content");
 
 //The different messages.
-var intro_message = "I'm an engineering student at "
+const intro_message = "I'm an engineering student at "
   .split("")
   .concat(["<a class=\"uniandes\" href=\"https://uniandes.edu.co/en\" target=\"_blank\">uniandes</a>"])
   .concat(" and I'm interested in various topics. Among my interests are literature, liguistics, music, information theory, generative grammars, 3d printing, and the list keeps growing..."
     .split(""));
-var contact_info = "You can find me at p.salazar12[at]uniandes.edu.co and @pedrito8472 on "
+const contact_info = "You can find me at p.salazar12[at]uniandes.edu.co and @pedrito8472 on "
   .split("")
   .concat(["<a class=\"twitter\" href=\"https://twitter.com/pedrito8472\" target=\"_blank\">twitter</a>"])
   .concat(".".split(""));
-var projects = "At the moment I'm working on an autonomous program grading tool called "
+const projects = "At the moment I'm working on an autonomous program grading tool called "
   .split("")
   .concat(["<a class=\"senecode\" href=\"#\">Senecode</a>"])
   .concat(" (link will be made public soon) which is to be used in the new Intro to Programming course at ".split(""))
